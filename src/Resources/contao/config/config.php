e9321b5d83351532da661e963d25ee372680ba44
Marc Haeser <marc.haeser@hinterhaushelden.de>
2021-01-03 17:38:49 +0100

    Create agencyReference frontend module

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,4 @@
+<?php
+
+// Frontend modules
+$GLOBALS['FE_MOD']['marchaeser']['ageny_reference'] = 'marchaeser\ContaoAgencyReferenceBundle\Module\AgencyReferenceModule';
---
+++ src/Resources/contao/languages/de/modules.php
@@ -0,0 +1,4 @@
+<?php
+
+// Frontend modules
+$GLOBALS['TL_LANG']['FMD']['agency_reference'] = ['Hello World', 'Gibt den Text "Hello World" aus'];
